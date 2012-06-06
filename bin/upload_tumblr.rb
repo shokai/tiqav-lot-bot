b0f32ccc3cc30f4ef5f2468c286c7848d6200e8c
Sho Hashimoto <hashimoto@shokai.org>
2012-06-06 14:59:18 +0900

    upload tumblr

---
+++ bin/upload_tumblr.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'bundler/setup'
+require File.expand_path 'bootstrap', File.dirname(__FILE__)+'/../'
+require 'tumblr'
+
+if ARGV.empty?
+  STDERR.puts 'image required.'
+  STDERR.puts "e.g.  ruby #{$0} image.jpg"
+  exit 1
+end
+
+file = ARGV.first
+
+puts "#{file} uploading.."
+res = Tumblr.new(Conf['tumblr']['mail'], Conf['tumblr']['pass']).
+  write_photo(file, Conf['description'])
+
+if res.to_i > 0
+  puts "success!! => #{res}"
+else
+  puts "failed."
+end
