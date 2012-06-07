3ddb3a7191d5ecb461fb86fd3b218bbd853dc894
Sho Hashimoto <hashimoto@shokai.org>
2012-06-08 05:47:41 +0900

    set width 500

--- bin/make_lot.rb
+++ bin/make_lot.rb
@@ -6,6 +6,8 @@ require 'mini_magick'
 require File.expand_path 'bootstrap', File.dirname(__FILE__)+'/../'
 require 'tiqav'
 
+WIDTH = 500
+
 if ARGV.empty?
   STDERR.puts 'filename required.'
   STDERR.puts "e.g.  ruby #{$0} image.jpg"
@@ -18,22 +20,15 @@ a = Tiqav.random
 b = Tiqav.random
 
 Dir.mktmpdir do |dir|
-  a_path = a.save File.expand_path a.filename, dir
-  b_path = b.save File.expand_path b.filename, dir
-  img_a = MiniMagick::Image.open a_path
-  img_b = MiniMagick::Image.open b_path
+  img_a = MiniMagick::Image.open a.save(File.expand_path a.filename, dir)
+  img_b = MiniMagick::Image.open b.save(File.expand_path b.filename, dir)
   
   puts "#{a.permalink} : #{img_a[:width]}x#{img_a[:height]}"
   puts "#{b.permalink} : #{img_b[:width]}x#{img_b[:height]}"
-  if img_a[:width] > img_b[:width]
-    img_a.resize "#{img_b[:width]}x#{(img_a[:height]*img_a[:width]/img_b[:width]).to_i}"
-    a_path = File.expand_path "resized.jpg", dir
-    img_a.write a_path
-  elsif img_a[:width] < img_b[:width]
-    img_b.resize "#{img_a[:width]}x#{(img_b[:height]*img_b[:width]/img_a[:width]).to_i}"
-    b_path = File.expand_path "resized.jpg", dir
-    img_b.write b_path
-  end
+  img_a.resize "#{WIDTH}x#{(img_a[:height]*WIDTH/img_a[:width]).to_i}"
+  img_a.write(a_path=File.expand_path("a_resized.jpg", dir))
+  img_b.resize "#{WIDTH}x#{(img_b[:height]*WIDTH/img_b[:width]).to_i}"
+  img_b.write(b_path=File.expand_path("b_resized.jpg", dir))
   system "convert -append #{a_path} #{b_path} #{out}"
 end
 
